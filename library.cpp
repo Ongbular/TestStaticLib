ad08bf4771e549188fe037056b6e21f65bc14678
ongbular <ongbular@gmail.com>
2022-10-28 18:51:07 +0900

    first commit

---
+++ library.cpp
@@ -0,0 +1,12 @@
+#include "library.h"
+#include <iostream>
+
+void hello()
+{
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int Add(int a, int b)
+{
+    return a + b;
+}
---
+++ library.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void hello();
+
+int Add(int a, int b);
